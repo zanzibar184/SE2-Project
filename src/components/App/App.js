63603e18e363209644b6d7fab22ad3816a8c2db0
vale17accidentidellastoria <pingupucca@gmail.com>
2017-11-21 11:54:16 +0100

    Add Youtube base integration

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+import YoutubePlayer from 'react-youtube-player';
 import ChatBot from 'react-simple-chatbot';
+
 
 import "./App.css";
 
@@ -10,6 +12,41 @@ const App = () => (
         <div className="row">
             <div className="col-md-8 Scrollbar Vertical-fit">
 
+                <div className="row thumbnail flex-row Container-multimedia First-media-color">
+                    <div className="col-lg-8">
+                        <div className="Youtube-dim">
+                            <YoutubePlayer videoId='hVwgHLGmqi4' />
+                        </div>
+                    </div>
+                    <div className="col-lg-4" />
+                </div>
+
+                <div className="row thumbnail flex-row Container-multimedia Second-media-color">
+                    <div className="col-lg-4"/>
+                    <div className="col-lg-8">
+                        <div className="Youtube-dim">
+                            <YoutubePlayer videoId='QTDmgsTLeTc' />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row thumbnail flex-row Container-multimedia First-media-color">
+                    <div className="col-lg-8">
+                        <div className="Youtube-dim">
+                            <YoutubePlayer videoId='yP6wCjJab6M' />
+                        </div>
+                    </div>
+                    <div className="col-lg-4" />
+                </div>
+
+                <div className="row thumbnail flex-row Container-multimedia Second-media-color">
+                    <div className="col-lg-4"/>
+                    <div className="col-lg-8">
+                        <div className="Youtube-dim">
+                            <YoutubePlayer videoId='k1-TrAvp_xs' />
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
