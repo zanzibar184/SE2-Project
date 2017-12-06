fe10335195b6ff0ccba65e6ba772a5b803cfe7bb
wizofoz <t0t3m1st@gmail.com>
2017-12-06 01:00:28 +0100

    Fixes asynchronous jest tests

--- __tests__/database-api.test.js
+++ __tests__/database-api.test.js
@@ -4,28 +4,36 @@ const db = new DatabaseAPI();
 
 describe('Server DatabaseAPI', () => {
 
-    test('adds youtube content', () => {
-        expect(db.addSharedContent('test@mail.com','jest',-1, 'youtube', 'youtube_url')).toBeTruthy();
+    test('adds youtube content', done => {
+        db.addSharedContent('test@mail.com','jest',123, 'youtube', 'youtube_url', (success) => {
+            expect(success).toBeTruthy();
+            done();
+        })
     });
 
-    test('find patient content with login email', () => {
+    test('find patient content with login email', done => {
 
         db.getPatientContents('test@mail.com','jest', (success, results) => {
-            expect(success).toBeTruthy();
+            expect(results.length).toBeGreaterThan(0);
+            done();
         });
 
     });
 
-    test('find patient content without login email', () => {
+    test('find patient content without login email', done => {
 
         db.getPatientContents(null, 'jest', (success, results) => {
-            expect(success).toBeTruthy();
+            expect(results.length).toBeGreaterThan(0);
+            done();
         });
 
     });
 
-    test('remove youtube content', () => {
-        expect(db.removeSharedContent('test@mail.com','jest',-1)).toBeTruthy();
+    test('remove youtube content', done => {
+        db.removeSharedContent('test@mail.com','jest',123, (success) => {
+            expect(success).toBeTruthy();
+            done();
+        })
     });
 
 });
--- __tests__/youtube-api.test.js
+++ __tests__/youtube-api.test.js
@@ -3,9 +3,10 @@ const YoutubeAPI = require.requireActual( '../server/YoutubeAPI');
 const youtube = new YoutubeAPI();
 
 describe('Server YoutubeAPI', () => {
-    test('search and retrieve 4 youtube videos', () => {
+    test('search and retrieve 4 youtube videos', done => {
         youtube.find('mozart', (results) => {
             expect(results.length).toEqual(4);
+            done();
         });
     });
 });
