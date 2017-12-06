2748880156ad2e03f62d535c3f6b700ffedea6e6
wizofoz <t0t3m1st@gmail.com>
2017-12-06 20:27:47 +0100

    Improves client-server api tests with promises

--- __tests__/client-server-api.js
+++ __tests__/client-server-api.js
@@ -3,34 +3,50 @@ const app = require('../server/app')
 
 describe('Client <-> Server API', () => {
 
-    test('search and retrieve 4 youtube videos', done => {
-        request(app).get('/api/youtube?search=mozart').then((response) => {
+    test('search and retrieve 4 youtube videos', () => {
+
+        return request(app).get('/api/youtube?search=mozart').then(response => {
             expect(response.statusCode).toBe(200);
             expect(JSON.parse(response.text).length).toEqual(4);
-            done();
         });
+
     });
 
-    test('adds youtube content', done => {
-        request(app).get('/api/db?action=add&email=test@mail.com&id_patient=jest&date=-1&content_id=youtube&content=youtube_url').then((response) => {
+    test('adds youtube content', () => {
+
+        return request(app).get('/api/db?action=add&email=test@mail.com&id_patient=jest&date=-1&content_id=youtube&content=youtube_url').then(response => {
             expect(response.statusCode).toBe(200);
-            done();
+            expect(response.text).toBeTruthy();
+        });
+
       });
+
+
+    test('find patient content with e-mail', () => {
+
+        return request(app).get('/api/db?action=get&email=test@mail.com&id_patient=jest').then(response => {
+            expect(response.statusCode).toBe(200);
+            expect(JSON.parse(response.text).length).toBeGreaterThan(0);
+        });
+
     });
 
-    test('find patient content', done => {
-        request(app).get('/api/db?action=get&email=test@mail.com&id_patient=jest').then((response) => {
+    test('find patient content without e-mail', () => {
+
+        return request(app).get('/api/db?action=get&id_patient=jest').then(response => {
             expect(response.statusCode).toBe(200);
-            //TODO: Adds array check
-            done();
+            expect(JSON.parse(response.text).length).toBeGreaterThan(0);
         });
+
     });
 
-    test('remove youtube content', done => {
-        request(app).get('/api/db?action=rem&email=test@mail.com&id_patient=jest&date=-1').then((response) => {
+    test('remove youtube content', () => {
+
+        return request(app).get('/api/db?action=rem&email=test@mail.com&id_patient=jest&date=-1').then(response => {
             expect(response.statusCode).toBe(200);
-            done();
+            expect(response.text).toBeTruthy();
         });
+
     });
 
 
