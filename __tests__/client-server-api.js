396188925ac9a7ef7d176fbd65b706ea66201139
vale17accidentidellastoria <pingupucca@gmail.com>
2017-12-07 18:51:36 +0100

    Adds more client-server api tests

--- __tests__/client-server-api.js
+++ __tests__/client-server-api.js
@@ -12,6 +12,60 @@ describe('Client <-> Server API', () => {
 
     });
 
+    test('does not add youtube content without email and patientID', () => {
+
+        return request(app).get('/api/db?action=add&date=-1&content_id=youtube&content=youtube_url').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('does not add youtube content without youtube_identifier and youtbe video url', () => {
+
+        return request(app).get('/api/db?action=add&email=test@mail.com&id_patient=jest&date=-1').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('does not add youtube content with youtube_identifier but without youtube video url', () => {
+
+        return request(app).get('/api/db?action=add&email=test@mail.com&id_patient=jest&date=-1&content_id=youtube').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('does not add youtube content with youtbe video url but without youtube_identifier', () => {
+
+        return request(app).get('/api/db?action=add&email=test@mail.com&id_patient=jest&date=-1&content=youtube_url').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('does not add youtube content without email', () => {
+
+        return request(app).get('/api/db?action=add&id_patient=jest&date=-1&content_id=youtube&content=youtube_url').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('does not add youtube content without patientID', () => {
+
+        return request(app).get('/api/db?action=add&email=test@mail.com&date=-1&content_id=youtube&content=youtube_url').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
     test('adds youtube content', () => {
 
         return request(app).get('/api/db?action=add&email=test@mail.com&id_patient=jest&date=-1&content_id=youtube&content=youtube_url').then(response => {
@@ -19,8 +73,7 @@ describe('Client <-> Server API', () => {
             expect(response.text).toBeTruthy();
         });
 
-      });
-
+    });
 
     test('find patient content with e-mail', () => {
 
@@ -40,6 +93,24 @@ describe('Client <-> Server API', () => {
 
     });
 
+    test('does not add patient content with e-mail but without patientID', () => {
+
+        return request(app).get('/api/db?action=get&email=test@mail.com').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('does not add patient content without e-mail and patientID', () => {
+
+        return request(app).get('/api/db?action=get').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
     test('remove youtube content', () => {
 
         return request(app).get('/api/db?action=rem&email=test@mail.com&id_patient=jest&date=-1').then(response => {
@@ -49,5 +120,58 @@ describe('Client <-> Server API', () => {
 
     });
 
+    test('does not remove youtube content without both email and patientID', () => {
+
+        return request(app).get('/api/db?action=rem&date=-1').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('does not remove youtube content without email but with patientID', () => {
+
+        return request(app).get('/api/db?action=rem&id_patient=jest&date=-1').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('does not remove youtube content with email but without patientID', () => {
+
+        return request(app).get('/api/db?action=rem&email=test@mail.com&date=-1').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('adds nothing without other parameters', () => {
+
+        return request(app).get('/api/db?action=add').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('removes nothing without other parameters', () => {
+
+        return request(app).get('/api/db?action=rem').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
+
+    test('gets nothing without other parameters', () => {
+
+        return request(app).get('/api/db?action=get').then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe("Parametri errati nella chiamata API.");
+        });
+
+    });
 
 });
