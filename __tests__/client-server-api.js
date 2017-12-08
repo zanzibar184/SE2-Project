75c81d6e6cf57e25ba8882060fe295528a86dc6b
wizofoz <t0t3m1st@gmail.com>
2017-12-08 10:32:08 +0100

    Improves test readability with common structures and functions TODO: update the tests in order to use these structures

--- __tests__/client-server-api.js
+++ __tests__/client-server-api.js
@@ -1,26 +1,74 @@
 const request = require('supertest');
-const app = require('../server/app')
+const app = require('../server/app');
+
+//----------------------------------------------------------------------------
+
+// equivalent to fetch with method: 'GET'
+const youtubeSearch = function(searchString) {
+    let url               = '/api/youtube?';
+    if(searchString) url += '?search='+ searchString;
+    return request(app).get(url);
+};
+
+//----------------------------------------------------------------------------
+
+const dbData = {
+    email: 'jest@kioku.com',        // Who shared the content
+    patient_id: 'jest',             // Patient ID ( identifies the same conversation session )
+    content_id: 'youtube',          // Shared content id (ex. 'youtube' identifies a youtube url)
+    content: 'yP6wCjJab6M',         // Shared content (ex. for content_id==='youtube' is the youtube video id
+    date: new Date().getTime()      // Number that identifies when the content was shared
+};
+
+// equivalent to fetch with method: 'GET'
+const dbCall = function (action, patient_id, date, content_id, content, email) {
+    let url             = '/api/db?';
+    if(action)     url += 'action=' + action + '&';
+    if(patient_id) url += 'id_patient=' + patient_id + '&';
+    if(date)       url += 'date=' + date + '&';
+    if(content_id) url += 'content_id=' + content_id + '&';
+    if(content)    url += 'content=' + content + '&';
+    if(email)      url += 'email=' + email;
+    return request(app).get(url);
+};
+
+//----------------------------------------------------------------------------
+
+const errorParameterMessage = 'Parametri errati nella chiamata API.';
+
+//----------------------------------------------------------------------------
 
 describe('Client <-> Server API', () => {
 
     test('search and retrieve 4 youtube videos', () => {
 
-        return request(app).get('/api/youtube?search=mozart').then(response => {
+        return youtubeSearch('mozart').then(response => {
             expect(response.statusCode).toBe(200);
             expect(JSON.parse(response.text).length).toEqual(4);
         });
 
     });
 
-    test('does not add youtube content without email and patientID', () => {
+    test('call youtube api without search parameter', () => {
 
-        return request(app).get('/api/db?action=add&date=-1&content_id=youtube&content=youtube_url').then(response => {
+        return youtubeSearch(null).then(response => {
             expect(response.statusCode).toBe(404);
-            expect(response.text).toBe("Parametri errati nella chiamata API.");
+            expect(response.text).toBe(errorParameterMessage);
         });
 
     });
 
+    test('does not add youtube content without email and patientID', () => {
+
+        // Per Valentina: l'originale era: request(app).get('/api/db?action=add&date=-1&content_id=youtube&content=youtube_url')
+        return request(dbCall('add', null, dbData.date, dbData.content_id, dbData.content, null)).then(response => {
+            expect(response.statusCode).toBe(404);
+            expect(response.text).toBe(errorParameterMessage);
+        });
+
+    });
+
+    // Per Valentina: sostituire da qui
     test('does not add youtube content without youtube_identifier and youtbe video url', () => {
 
         return request(app).get('/api/db?action=add&email=test@mail.com&id_patient=jest&date=-1').then(response => {
--- __tests__/database-api.test.js
+++ __tests__/database-api.test.js
@@ -2,15 +2,28 @@ const DatabaseAPI = require.requireActual( '../server/DatabaseAPI');
 
 const db = new DatabaseAPI();
 
+//-----------------------------------------------------
+
+const dbData = {
+    email: 'jest@kioku.com',        // Who shared the content
+    patient_id: 'jest',             // Patient ID ( identifies the same conversation session )
+    content_id: 'youtube',          // Shared content id (ex. 'youtube' identifies a youtube url)
+    content: 'yP6wCjJab6M',         // Shared content (ex. for content_id==='youtube' is the youtube video id
+    date: new Date().getTime()      // Number that identifies when the content was shared
+};
+
+//-----------------------------------------------------
+
 describe('Server DatabaseAPI', () => {
 
     test('adds youtube content', done => {
-        db.addSharedContent('test@mail.com','jest',123, 'youtube', 'youtube_url', (success) => {
+        db.addSharedContent(dbData.email, dbData.patient_id, dbData.date, dbData.content_id, dbData.content, (success) => {
             expect(success).toBeTruthy();
             done();
         })
     });
 
+    //Per Valentina: sostituire da qui
     test('find patient content with login email', done => {
 
         db.getPatientContents('test@mail.com','jest', (success, results) => {
